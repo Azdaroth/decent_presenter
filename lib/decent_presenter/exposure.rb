1c7ca6ea0156828be966f6d81af4df705736ebd3
Karol Galanciak <karol.galanciak@gmail.com>
2015-03-29 14:25:21 +0200

    presentable is a collection if responds to :to_ary

--- lib/decent_presenter/exposure.rb
+++ lib/decent_presenter/exposure.rb
@@ -2,7 +2,7 @@ require "decent_presenter/collection_proxy"
 
 module DecentPresenter
   class Exposure
-    
+
     attr_reader :view_context, :presenter_factory
     private :view_context, :presenter_factory
 
@@ -31,10 +31,10 @@ module DecentPresenter
       def present_collection(collection, options = {})
         presented_collection = collection.map { |el| present_model(el, options) }
         DecentPresenter::CollectionProxy.new(collection, presented_collection)
-      end      
+      end
 
       def presentable_is_a_collection?(presentable)
-        [:size, :to_a, :first].all? { |method| presentable.respond_to? method }
+        presentable.respond_to? :to_ary
       end
 
   end
